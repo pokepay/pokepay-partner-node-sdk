6f169a97088a78d51cbedcdb55dd9106361153dc
Satoshi Imai <satoshi.imai@gmail.com>
2022-12-22 21:43:47 +0900

    Build ts files

---
+++ dist/response/AccountTransferSummary.d.ts
@@ -0,0 +1,5 @@
+import { AccountTransferSummaryElement } from "./AccountTransferSummaryElement";
+interface AccountTransferSummary {
+    readonly summaries: AccountTransferSummaryElement[];
+}
+export { AccountTransferSummary };
